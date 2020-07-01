99ffde30f6e3ed97915ae55c0c5f82534f5d58d5
kareemradwan <kradwan@students.iugaza.edu.ps>
2020-07-01 13:45:46 +0300

    minSdk = 15 , add round circular to step indicator , remove forground for step

--- stepeer/src/main/java/com/kradwan/stepeer/model/StepColor.kt
+++ stepeer/src/main/java/com/kradwan/stepeer/model/StepColor.kt
@@ -3,7 +3,6 @@ package com.kradwan.stepeer.model
 data class StepColor(var color: Int){
 
     companion object{
-
         const val COLOR_CHECKED = "checked_color"
         const val COLOR_UNCHECKED = "unchecked_color"
     }
---
+++ stepeer/src/main/java/com/kradwan/stepeer/view/RoundedCornerLayout.java
@@ -0,0 +1,49 @@
+package com.kradwan.stepeer.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class RoundedCornerLayout extends FrameLayout {
+
+
+    public RoundedCornerLayout(Context context) {
+        this(context, null, 0);
+    }
+
+    public RoundedCornerLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundedCornerLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    public double getCornerRadius() {
+        return 20;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        int count = canvas.save();
+
+        final Path path = new Path();
+        path.addRoundRect(new RectF(0, 0, getWidth(), getHeight()), (float) getCornerRadius(), (float) getCornerRadius(), Path.Direction.CW);
+        canvas.clipPath(path);
+
+        canvas.clipPath(path);
+        super.draw(canvas);
+        canvas.restoreToCount(count);
+    }
+}
